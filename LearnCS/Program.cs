bf532d70dbfc76b8797df57d8051935aa1d8547f
Midhushan <39880516+Midhushan@users.noreply.github.com>
2020-01-10 06:02:29 -0800

    Add files via upload

---
+++ LearnCS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LearnCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 22 / 7;
+            double r;
+            Console.WriteLine("Enter Rdius : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi * r * r;
+            Console.WriteLine("Radius: {0}, Area: {0}", areaCircle);
+            Console.WriteLine();
+
+
+        }
+    }
+}
