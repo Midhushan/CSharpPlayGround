71fde3fb4aacd3ccd34e72007e5bcf2d588ff4d1
Ninja <39880516+Midhushan@users.noreply.github.com>
2020-01-17 07:53:06 +0530

    Added new excercise - Generics

---
+++ Genericsample/Genericsample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericsample
+{
+    class myclass<T>
+    {
+        public bool compare(T a, T b)
+            {
+            if (a.Equals(b))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myclass<int> value = new myclass<int>();
+            bool result = value.compare(10, 5);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
